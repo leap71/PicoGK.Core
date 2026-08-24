5ce76510262e7cf578c7ce542bb28049a33ec4da
Lin Kayser <148448288+LinKayser@users.noreply.github.com>
2026-08-24 19:09:17 +1200

    Update AssemblyInfo.cs

--- tests/PicoGK.Core.Tests/AssemblyInfo.cs
+++ tests/PicoGK.Core.Tests/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
 using Xunit;
 
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
+[assembly: Parallelization(Mode = ParallelizationMode.None)]
